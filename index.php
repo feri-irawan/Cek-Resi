d4865ee95e2e942c4f49be04c1126c261e6b093e
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-29 15:20:02 +0800

    update index.php

--- index.php
+++ index.php
@@ -18,9 +18,17 @@ print_r($file);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Cek Resi</title>
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
 </head>
 <body>
+  
+  <form action="" method="get" accept-charset="utf-8">
+    <input class="form-control mb-3" type="text" name="resi" placeholder="Masukan No. Resi..."/>
+    <button class="btn btn-primary" type="submit">Cek</button>
+  </form>
   
 </body>
 </html>
