6f1c4f0ebd07dce9790f7e807d6c6d20ada67a8d
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-29 13:54:02 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
+$file = file_get_contents("https://api.binderbyte.com/v1/track?api_key=8e49f28e0f2f2cf56393c352613eec358e85fb7077ce6f7f453ebb826a7b1f6d&courier=jne&awb=8825112045716759");
+$file = json_decode($file, true);
 
-$data = [
-    "nama" => "Feri irawan",
-    "kelas" => 8,
-    "gender" => "pria"
-  ];
-  
-var_dump($data)
-?>
+print_r($file);
