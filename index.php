a35757e876bd979bacaba5d1423e601b6c4535cc
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-29 14:03:41 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 //login
-$file = file_get_contents("https://resi.id/api/auth/login");
+$email = "pixwebsite1998@gmail.com";
+$pass  = "@qazxcvbn123";
+
+$file = file_get_contents("https://resi.id/api/auth/login?email=$email&password=$pass");
 $file = json_decode($file, true);
 
 var_dump($file);
