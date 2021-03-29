e0ce54f2138ab809532f5cfa5fb1d1c5d99034d1
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-29 19:58:12 +0800

    update index.php

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ $file = json_decode($file, true);
       <th colspan="2" class="text-center">Detail</th>
     </tr>
     <tr>
-      <td width="100px">Dari</td>
+      <td>Dari</td>
       <td><?= info("detail", "origin") ?></td>
     </tr>
     <tr>
