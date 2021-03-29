a2ce3793944b8c420ab4b0608a423cf790497205
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-29 14:51:35 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
-<?php
-
-$url = "https://resi.id/api/auth/login?email=pixwebsite1998@gmail.com&password=@qazxcvbn123";
-
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-$headers = array(
-   "Content-Type: application/json",
-   "Content-Length: 0",
-);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-//for debug only!
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-$resp = curl_exec($curl);
-curl_close($curl);
-var_dump($resp);
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <form action="https://resi.id/api/auth/login" method="post" accept-charset="utf-8">
+    
+  </form>
+</body>
+</html>
