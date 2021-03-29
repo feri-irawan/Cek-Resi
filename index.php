d70c3229872947fabb46b0368180c46ceb2fe451
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-29 15:17:08 +0800

    update index.php

--- index.php
+++ index.php
@@ -9,3 +9,18 @@ $file = file_get_contents($url_api);
 $file = json_decode($file, true);
 
 print_r($file);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
