657a4fef12ae65a90e337d335a96251707985a88
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-29 15:16:43 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $api_key = "b0b6beed4cef4b155d14a9514fafd24e9797e3e075f81f488bc20fc53153a900";
 $courier = $_GET["courier"];
-$no_resi  = $_GET["resi"];
-$param  = "api_key=$api_key&courier=$courier&awb=$no_resi";
+$no_resi = $_GET["resi"];
+$params  = "api_key=$api_key&courier=$courier&awb=$no_resi";
 
-$url_api = "https://api.binderbyte.com/v1/track?$param";
+$url_api = "https://api.binderbyte.com/v1/track?$params";
 $file = file_get_contents($url_api);
 $file = json_decode($file, true);
 
