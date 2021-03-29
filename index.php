9e13f699062326d839a251bd1b2daf2fbb1baeab
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-29 14:02:32 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-$file = file_get_contents("https://api.binderbyte.com/v1/track?api_key=8e49f28e0f2f2cf56393c352613eec358e85fb7077ce6f7f453ebb826a7b1f6d&courier=jne&awb=8825112045716759");
+//login
+$file = file_get_contents("https://resi.id/api/auth/login");
 $file = json_decode($file, true);
 
 var_dump($file);
