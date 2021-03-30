a0887487aee903c78e59c259a0119adeb57352ac
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-30 08:44:34 +0800

    update index.php

--- index.php
+++ index.php
@@ -20,6 +20,13 @@ $file = json_decode($file, true);
   
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
+<style>
+.box-circle {
+  display: inline-block;
+  border-radius: 20px;
+  background: #fff;
+}
+</style>
 </head>
 <body>
   
