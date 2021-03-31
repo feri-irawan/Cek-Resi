d82509d1a01c8f60f4bd9eb8281f2816e3d96e96
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-31 12:46:34 +0800

    update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ th {
           <option value="<?=$rowCourier["code"]?>"><?=$rowCourier["description"]?></option>
         <?php endforeach; ?>
       </select>
-      <button class="btn btn-primary" type="submit">Cek</button>
+      <button class="btn btn-danger" type="submit">Cek</button>
      </div>
    </div>
   </form>
