c68cd3d6e7fb4418c303bd96319e5c7f445e8136
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-03-31 11:45:37 +0800

    update index.php

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ $file = json_decode($file, true);
       <div class="card mb-3">
         <div class="card-header bg-primary text-white">
           <div class="box-circle"><?=$i?></div>
-          <?= $row["date"] ?>
+          <?= history("date") ?>
         </div>
         <div class="card-body"><?= history("desc") ?></div>
         <div class="card-footer">
